2577a41b64e6d0c8a518cbd88133a8c947b79cf0
David <fatokundavid2@gmail.com>
2023-07-02 23:43:25 -0600

    LighthouseBnB Add Property

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -217,10 +217,8 @@ const addProperty = function (property) {
   // return Promise.resolve(property);
 
   return client
-    .query(`INSERT INTO properties (id, owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city,
-      province, post_code, country, parking_spaces, number_of_bathrooms,
-      number_of_bedrooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING *;
-`, [property.id, property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+    .query(`INSERT INTO properties (id, owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING *;
+`, [property.id, property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code, property.active])
     .then((result) => {
       console.log(result.rows);
       return result.rows;
